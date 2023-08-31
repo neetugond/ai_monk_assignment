3bb2a5cf0b274cfdb2fa153cbec1d403c31034c3
neetugond <ngond92@gmail.com>
2023-08-31 17:07:55 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Tagview from './components/Tagview';
 
+const initialTree = {
+  id: uuidv4(),
+  name: 'root',
+  children: [
+    {
+      id: uuidv4(),
+      name: 'child1',
+      children: [
+        {id: uuidv4(), name: 'child1-child1', data: 'c1-c1 Hello' },
+        { id: uuidv4(),name: 'child1-child2', data: 'c1-c2 JS' },
+      ],
+    },
+    { id: uuidv4(), name: 'child2', data: 'c2 World' },
+  ],
+};
 
 function App() {
-  const initialTree = {
-    name: 'root',
-    children: [
-      {
-        name: 'child1',
-        children: [
-          { name: 'child1-child1', data: 'c1-c1 Hello' },
-          { name: 'child1-child2', data: 'c1-c2 JS' },
-        ],
-      },
-      { name: 'child2', data: 'c2 World' },
-    ],
+  const [treeData, setTreeData] = useState(initialTree);
+  const [content, setContent] = useState('')
+
+  function removeid(obj) {
+    if (Array.isArray(obj)) {
+      return obj.map(el => removeid(el));
+    } else if (typeof obj === 'object' && obj != null) {
+      const { id, ...rest } = obj;
+      return Object.fromEntries(
+        Object.entries(rest).map(([key, value]) => [key, removeid(value)])
+      );
+    }
+    return obj;
+  }
+  const handleExport = () => {
+    const exportObjData = removeid(treeData);
+    setContent(JSON.stringify(exportObjData, null, 2));
   };
+    
   return (
     <div className="App">
-        <Tagview tag={initialTree} />
+      <Tagview tag={initialTree} />
+      <button onClick={handleExport}>Export</button>
+      <div>{ content}</div>
     </div>
   );
 }
--- src/components/Tagview.jsx
+++ src/components/Tagview.jsx
@@ -2,29 +2,44 @@ import React, { useState } from 'react'
 import './style.css'
 
 
-function Tagview({ tag, onaddChild }) {
+function Tagview({ tag}) {
+    const [dropdown, setDropdown] = useState(false)
     const [data, setData] = useState(tag.data || '');
   const [children, setChildren] = useState(tag.children || []);
 
   const handleAddChild = () => {
-    const newChild = {
-      name: `child${children.length + 1}`,
-    };
+    const newChild = { name: 'new-child', data: '' };
     setChildren([...children, newChild]);
   };
+    const handleDropdownToggle = () => {
+        setDropdown(!dropdown);
+    };
+
     return (
         <>
         <div className="tag">
-          <div className="tag-header">
-            <span>{tag.name}</span>
-            <button onClick={handleAddChild}>Add Child</button>
-          </div>
-          <div className="tag-data">
-            {children.map((child, index) => (
-              <Tagview key={index} tag={child} onAddChild={handleAddChild} />
-            ))}
-            {data && <p>{data}</p>}
-          </div>
+                <div className="tag-header">
+                    <div className='tag-left'>
+                    <button onClick={handleDropdownToggle}>{dropdown ? '>' : 'v'} </button>
+                    <span>{tag.name}</span>
+                    </div>
+                    
+                    <button onClick={handleAddChild}>Add Child</button>
+                    
+                </div>
+                {!dropdown && (
+                    <div className="tag-data">
+                        <label>Data </label>
+                        <input type="text" value={data}  onChange={(e) => {
+                    setData(e.target.value);
+                     }} />
+                    {children.map((child, index) => (
+                      <Tagview key={index} tag={child} onAddChild={handleAddChild} />
+                    ))}
+                  </div>
+
+                )}
+          
         </div>
         </>
       );
